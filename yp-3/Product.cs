a0813c4da6ca98ad2e565f40e9c549ff6daded98
Mumlul <152785774+Mumlul@users.noreply.github.com>
2024-11-07 16:36:23 +0500

    Add files via upload

---
+++ yp-3/Product.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace yp_3
+{
+    public class Product
+    {
+        // Поля
+        protected string name;
+        protected double price;
+        protected int count;
+        protected string art;
+        protected string color;
+
+        // Конструктор
+        public Product(string name, double price,int count,string art,string color)
+        {
+            this.name = name;
+            this.price = price;
+            this.count = count;
+            this.art = art;
+            this.color = color;
+        }
+
+        // Функция определения качества
+        public virtual double CalculateQuality()
+        {
+            return price/count; 
+        }
+
+        // Вывод информации
+        public virtual string DisplayInfo()
+        {
+            return $"Name: {name} Price: {price:C} Count: {count}  Art:{art} Color:{color}";
+        }
+
+        // Перегрузки методов добавления
+        public static List<Product> AddProduct(List<Product> products, Product product)
+        {
+            products.Add(product);
+            return products;
+        }
+
+        public static List<Product> AddProduct(List<Product> products, string name, double price,int count, string art, string color)
+        {
+            products.Add(new Product(name, price,count, art, color));
+            return products;
+        }
+
+        public static Queue<Product> AddProduct(Queue<Product> products, Product product)
+        {
+            products.Enqueue(product);
+            return products;
+        }
+
+        // Перегрузки методов удаления
+        public static List<Product> RemoveProduct(List<Product> products, Product product)
+        {
+            products.Remove(product);
+            return products;
+        }
+
+        public static List<Product> RemoveProduct(List<Product> products, string name)
+        {
+            products.RemoveAll(p => p.name == name);
+            return products;
+        }
+
+        public static Queue<Product> RemoveProduct(Queue<Product> products)
+        {
+            products.Dequeue();
+            return products;
+        }
+
+
+
+    }
+}
---
+++ yp-3/SpecialProduct.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yp_3
+{
+    public class SpecialProduct : Product
+    {
+        // Дополнительное поле P
+        private double popularity;
+
+        // Три новых свойства
+        public string Category { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public SpecialProduct(string name, double price,int count, string art, string color, double popularity, string category)
+            : base(name, price,count,art,color)
+        {
+            this.popularity = popularity;
+            Category = category;
+            ExpiryDate = DateTime.Now.AddMonths(6);
+            IsAvailable = true;
+        }
+
+        // Переопределенная функция качества
+        public override double CalculateQuality()
+        {
+            int curyear = DateTime.Now.Year;
+            int razr = ExpiryDate.Year;
+            return base.CalculateQuality() + 0.5*(curyear-razr);
+        }
+
+        // Собственная функция(скидка)
+        public double CalculateDiscount()
+        {
+            return price * (popularity / 100);
+        }
+
+        // Переопределенный вывод информации
+        public override string DisplayInfo()
+        {
+            base.DisplayInfo();
+            return $"Popularity: {popularity} Category: {Category} Expiry Date: {ExpiryDate:d}  Available:{IsAvailable} Discount:{CalculateDiscount():C}";
+        }
+    }
+}
